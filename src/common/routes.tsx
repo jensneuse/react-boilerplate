b96fbb38aee4ee641ce8f931d0e8b65331449764
Jens <jens.neuse@gmx.de>
2017-10-11 09:52:59 +0200

    ssr done

--- src/common/routes.tsx
+++ src/common/routes.tsx
@@ -11,7 +11,7 @@ import Stuff from "../common/components/AsyncStuff"
 class Routes extends React.Component<{},{}> {
     render(){
         return ([
-            <Route key="home" exact path="/" component={Home}/>,
+            <Route key="home" exact path="/" component={Home} />,
             <Route key="about" exact path="/about" component={About}/>,
             <Route key="stuff" exact path="/stuff" component={Stuff}/>
         ])
