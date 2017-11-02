e9810a8361de28dc01eb80c0fdf8ee9a48881526
Jens <jens.neuse@gmx.de>
2017-11-02 14:26:19 +0100

    add Dockerfile

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require("webpack");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CompressionPlugin = require("compression-webpack-plugin");
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const StatsWriterPlugin = require("webpack-stats-plugin").StatsWriterPlugin;
